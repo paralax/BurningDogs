991ff79365571dcf6c657e010364e0ba138de872
jose nazario <jose.monkey.org@gmail.com>
2016-12-30 21:42:02 -0500

    implement yarascan function and data

--- yara.fs
+++ yara.fs
@@ -12,7 +12,11 @@ type YaraString = {
 }
 
 type YaraMeta = {
-    Description : string
+    Description : string;
+    Author : string;
+    Date : System.DateTime;
+    Reference : string;
+    Filetype : string;
 }
 
 type YaraMatches = {
@@ -39,12 +43,48 @@ suspicious_packer_section [packer,PE] [author="@j0sm1",date="2016/10/21",descrip
 0x3ab7ce8:$s61: .yP
 0x4912dcb:$s61: .yP
 0x4e11b65:$s61: .yP
-
 *)
+
 let yarascan (rulefile: string) (filename: string) : YaraMatches =
+    let yaraStrings(input: string []) : YaraString list = 
+        let splitLine (line: string) : YaraString =
+            let [|offset;name;data|] = line.Split([|':'|])
+            {Offset=offset |> int;
+            Name=name;
+            Data=data.Trim()}
+        input |> Array.map splitLine |> List.ofArray
+
+    let yaraMeta(input: string) : YaraMeta =
+        let meta = input.Split(',') 
+                |> Array.map (fun x -> x.Split('=')) 
+                |> Array.map (fun x -> (x.[0],x.[1].Replace("\"", ""))) 
+                |> Map.ofArray
+        let getByKey(key: string) (meta : Map<string,string>) : string =
+            match Map.tryFind key meta with
+            | Some(x) -> x
+            | None    -> ""
+        let metaDate (date: string) : System.DateTime = 
+            match date with 
+            | "" -> System.DateTime.UtcNow
+            | _  -> System.DateTime.Parse date
+        {Description=getByKey "description" meta;
+        Author=getByKey "author" meta;
+        Date=getByKey "date" meta |> metaDate;
+        Reference=getByKey "reference" meta;
+        Filetype=getByKey "filetype" meta}
+
     let p = new Diagnostics.Process()
     p.StartInfo.FileName <- "/usr/local/bin/yara"
     p.StartInfo.Arguments <- String.Format("-sgm {0} {1}", rulefile, filename)
     p.StartInfo.UseShellExecute <- false
     p.Start() |> ignore
     let output = p.StandardOutput.ReadToEnd().Split([|'\n'|]) // parse
+    let paren = System.Text.RegularExpressions.Regex("\[[^]]*\]")
+    let matches = paren.Matches(output.[0])
+    let matchToValue (m : System.Text.RegularExpressions.Match) = m.Value.TrimStart('[').TrimEnd(']')
+    let ms = Seq.map matchToValue (Seq.cast matches)
+    {Rule=output.[0].Split(' ').[0];
+     Namespace="";
+     Meta=Seq.skip 1 ms |> Seq.head |> yaraMeta;
+     Tags=(Seq.head ms).Split([|','|]) |> List.ofArray;
+     Strings=output.[1..] |> yaraStrings}
